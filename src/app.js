4d5810b002858a69d858104827b6333b10bedc39
PearThanchat <thanchat.jan@gmail.com>
2024-02-03 16:04:47 +0700

    add nodemon

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('Hello')
+const express = require('express');
+const app = express();
+// Define a route 
+app.get('/', (req, res) => {
+    res.send('Hello Pear ka');
+});
+//start server
+app.listen(3000, () => {
+    console.log('server is listening on port 3000');
+});
