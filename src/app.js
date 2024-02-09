f25716c213176afd86de0aed410c174808c2d894
PearThanchat <thanchat.jan@gmail.com>
2024-02-09 21:33:59 +0700

    1.1 id:2

--- src/app.js
+++ src/app.js
@@ -4,6 +4,14 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello Pear ka');
 });
+
+app.get('/test', (req, res) => {
+    const id = req.query.id;
+
+    const output = `id: ${id}`;
+    res.send(output);
+});
+
 //start server
 app.listen(3000, () => {
     console.log('server is listening on port 3000');
